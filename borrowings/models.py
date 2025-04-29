5cb704cbc0ecf1590bc596035779f05887b9f02a
Amine Mastouri <amin.mastouri01@gmail.com>
2025-04-29 04:44:15 +0100

    creating  borrowing model logic

---
+++ borrowings/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+from books.models import Book
+from users.models import User
+
+class Borrowing(models.Model):
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now() + timedelta(days=30))
+
+    def save(self, *args, **kwargs):
+        if self.book.stock <= 0:
+            raise ValueError("Book not available")
+        if Borrowing.objects.filter(borrower=self.borrower, end_date__gte=timezone.now()).count() >= 5:
+            raise ValueError("Borrowing limit reached")
+        self.book.stock -= 1
+        self.book.save()
+        super().save(*args, **kwargs)
+
+    def is_active(self):
+        return self.end_date >= timezone.now()
+
+    def time_left(self):
+        return (self.end_date - timezone.now().date()).days
