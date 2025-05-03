ee0df115b2c5220410e263c4f033c2d03e5a6090
Amine Mastouri <amin.mastouri01@gmail.com>
2025-05-03 04:36:55 +0100

    fixing dockerfile entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 set -e
 
 echo "Applying database migrations..."
+python manage.py makemigrations users
+python manage.py makemigrations books
+
+python manage.py makemigrations borrowings
+
 python manage.py makemigrations
 python manage.py migrate
 
